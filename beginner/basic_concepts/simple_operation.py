7804855e692c81dda8b22971187d9a140f573082
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-08 14:36:44 +0700

    Update simple_operation.py
    Simple operation (+ - * /)

--- beginner/basic_concepts/simple_operation.py
+++ beginner/basic_concepts/simple_operation.py
@@ -1,9 +1,9 @@
 print("Hello "+'World!')
-print("Hi"*3)
-print("Hi "+"2")   #string
-print(2+2)   #int. also int using *
-print(2+2.0)   #float. Also float using *
-print(4/2)   #float. / = float
+print("Hi"*3)             #print Hi 3 times
+print("Hi "+"2")          #string
+print(2+2)                #int. also int using *
+print(2+2.0)              #float. Also float using *
+print(4/2)                #float. / = float
 
 print('''---Result---
 Hello World!
@@ -15,6 +15,7 @@ Hi 2
 ''')
 
 print{'''--Note--
-- print("Hello "+2) -->syntaxerror. Text data types can't do (+,- or /) with number data types, but can do (*).
-
+- print("Hello "+2) --> SyntaxError. Text data types can't do (+,- or /) with number data types, but can do (*).
+- every number calculation involves any float, always results in float
+- every number calculation involves (/), always results in float
 ''')
