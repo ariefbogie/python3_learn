f7d5110fe31b357c29779a1affddca9908859f2d
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-08 14:13:48 +0700

    Create printing_text.py
    Printing text

---
+++ beginner/basic_concepts/printing_text.py
@@ -0,0 +1,19 @@
+print("Hello\nWorld !")   #\n = enter
+print('123')   #all 2 = string
+print(123)   #int
+print(123.0)   #float
+
+print('''---Result---
+Hello
+World !
+123
+123
+123.0
+''') #this is multiline, everything is considered as string
+
+print('''--Note--
+- phyton read code per line. print('hi') print('yo') --> syntaxerror
+- assign single quote (') or double quote (") to print text data types
+- assign triple single quotes (') or double quotes (") to print multiline
+- assign no quote for number data types
+''')
