2ea4c20677f0c1a14673998445b38cd0866f8664
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-08 14:22:03 +0700

    Update simple_operation.py

--- beginner/basic_concepts/simple_operation.py
+++ beginner/basic_concepts/simple_operation.py
@@ -5,13 +5,16 @@ print(2+2)   #int. also int using *
 print(2+2.0)   #float. Also float using *
 print(4/2)   #float. / = float
 
+print('''---Result---
 Hello World!
 HiHiHi
 Hi 2
 4
 4.0
 2.0
+''')
 
+print{'''--Note--
+- print("Hello "+2) -->syntaxerror. Text data types can't do (+,- or /) with number data types, but can do (*).
 
-** Process exited - Return Code: 0 **
-Press Enter to exit terminal
+''')
