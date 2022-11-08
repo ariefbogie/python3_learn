0b613bb1253b0580313ca59446935a7b88fbd27d
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-08 15:54:01 +0700

    Create variable_types.py

---
+++ beginner/variables/variable_types.py
@@ -0,0 +1,12 @@
+my_user = 'Bogie'   #string
+print(my_user + '!')   #same rule as data_type
+name = input('Enter name: ')   #a prompt message 'Enter name: ' will appear. Default data_type = string
+age = int(input())   #change data_type input to int
+x = '2'
+y = '4'
+z = int(x) + int(y)   #change to int
+print(z)   #will calculate as int
+print(x + ' and ' + y)   #string operation
+---result--- 
+6
+2 and 4
