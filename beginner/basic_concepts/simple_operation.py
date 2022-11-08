ab06caa8f6d64eed57d4e858a5ca353cd7cf3516
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-08 14:18:23 +0700

    Create simple_operation.py
    Simple operation (+ - * /)

---
+++ beginner/basic_concepts/simple_operation.py
@@ -0,0 +1,17 @@
+print("Hello "+'World!')
+print("Hi"*3)
+print("Hi "+"2")   #string
+print(2+2)   #int. also int using *
+print(2+2.0)   #float. Also float using *
+print(4/2)   #float. / = float
+
+Hello World!
+HiHiHi
+Hi 2
+4
+4.0
+2.0
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
