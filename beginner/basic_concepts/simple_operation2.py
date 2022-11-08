62f462dd6d2b460d914b9093e299fa24058cffd5
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-08 14:54:20 +0700

    Create simple_operation2.py

---
+++ beginner/basic_concepts/simple_operation2.py
@@ -0,0 +1,19 @@
+print(2**4)   #int. exponent
+print(16**(1/2))   #float. (/) inside parentheses
+print(10//3)   #int. quotient
+print(10%3)   #int. remainder
+import math
+print(math.sqrt(16))
+
+print('''---Result---
+16
+4.0
+3
+1
+4.0
+''')
+
+print('''--Note--
+- 2nd line is printing square root of 16. exponent of half = square root. result in float
+- you can also use sqrt() function, but need import system. also result in float
+''')
