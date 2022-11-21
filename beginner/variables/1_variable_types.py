289a4fa304509225182a4121b833b864c13cd34b
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-21 20:32:55 +0700

    Update 1_variable_types.py

--- beginner/variables/1_variable_types.py
+++ beginner/variables/1_variable_types.py
@@ -1,12 +1,14 @@
-my_user = 'Bogie'   #string
-print(my_user + '!')   #same rule as data_type
-name = input('Enter name: ')   #a prompt message 'Enter name: ' will appear. Default data_type = string
-age = int(input())   #change data_type input to int
+my_user = 'Bogie'               #string
+print(my_user + '!')            #same rule as data_type
+name = input('Enter name: ')    #a prompt message 'Enter name: ' will appear. Default data_type = string
+age = int(input())              #change data_type input to int
+age2 = input('Enter age: ')     #string
+age3 = int(age2)                #change age 2 to int
 x = '2'
 y = '4'
-z = int(x) + int(y)   #change to int
-print(z)   #will calculate as int
-print(x + ' and ' + y)   #string operation
+z = int(x) + int(y)             #change to int
+print(z)                        #will calculate as int
+print(x + ' and ' + y)          #string operation
 
 print('''---result--- 
 6
