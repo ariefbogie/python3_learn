13b1bf4788f6399b89603375b8c5b8cec43e182c
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-22 09:01:17 +0700

    Update 1_boolean.py

--- beginner/control_flow/1_boolean.py
+++ beginner/control_flow/1_boolean.py
@@ -1,9 +1,15 @@
 print(2==3)   #return false
 print('hi'=='hi')   #true
+print(5>2 and 3>6)   #return false
+print(5>2 or 3>6)   #false
+print(not 5>2 or 3>6) #true, reverse
 
 print('''---result--- 
 False
 True
+False
+True
+False
 ''')
 
 print('''note:
