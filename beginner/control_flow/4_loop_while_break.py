642015adb8d473736130b2c58107a528bb2d6192
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-22 09:12:10 +0700

    Create 4_loop_while_break.py

---
+++ beginner/control_flow/4_loop_while_break.py
@@ -0,0 +1,21 @@
+i = 0
+while True:               #default: true
+   print(i)
+   i = i + 1
+   if i == 3 :
+      print('Breaking')
+      break
+print('Finished')
+
+print('''---result--- 
+0
+1
+2
+Breaking
+Finished
+''')
+
+print('''note:
+- remember to add a colon : after ehile condition
+- Case study: selecting a suitable GYM plan. Use when needed to loop as far as possible but stop the loop when already fit the rule
+''')
