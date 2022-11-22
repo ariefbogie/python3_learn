7e684139991b7b8eb68d1a38faf6c7be8535a7ad
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-22 08:58:57 +0700

    Create 3_nested_if.py

---
+++ beginner/control_flow/3_nested_if.py
@@ -0,0 +1,23 @@
+x = 10
+if x > 5: 
+   print('x > 5')         #LEVEL 1
+   print('hi')
+   if x > 11:
+      print('x > 11')     #level 2
+  
+print('''---result--- 
+x > 5
+hi
+''')
+
+print('''note:
+-You can write 'if x>5' or 'if (x>5)'
+-You can write with or without {}
+-you can also use else statement of elif (else if)
+-REMEMBER..!! the colon : both for if and else
+-indentation (space) is needed to define the level of nesting. example:
+x = 10
+if x > 5:
+   print('x > 5')
+    print('hi')         #return error here
+''')
