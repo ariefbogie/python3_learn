1761348d64af1ebdccd4813b58331632da3bacea
ariefbogie <110711761+ariefbogie@users.noreply.github.com>
2022-11-22 09:44:43 +0700

    Update 4_loop_while_break.py

--- beginner/control_flow/4_loop_while_break.py
+++ beginner/control_flow/4_loop_while_break.py
@@ -17,5 +17,6 @@ Finished
 
 print('''note:
 - remember to add a colon : after ehile condition
+- while break may end the program right when the conditions match, continue will not stop it but still excluded the matched condition
 - Case study: selecting a suitable GYM plan. Use when needed to loop as far as possible but stop the loop when already fit the rule
 ''')
